bccc9636d7aba778ee803977c29e73f425d796c4
Jerry W <jerrywiltse@gmail.com>
2018-01-04 16:03:14 -0500

    fixed test package by applying change from upstream

--- conanfile.py
+++ conanfile.py
@@ -6,16 +6,16 @@ from conans import ConanFile, CMake, tools
 
 
 class ParsonConan(ConanFile):
-    name = "Parson"
+    name = "parson"
     version = "0.1.0"
     homepage = "https://github.com/kgabis/parson"
     url = "https://github.com/bincrafters/conan-parson"
     description = "Lightweight JSON library written in C."
     license = "MIT"
     exports = ["LICENSE.md"]
+    exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
-    exports_sources = ["CMakeLists.txt"]
     options = {"shared": [True, False]}
     default_options = "shared=False"
 
